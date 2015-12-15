04a975238ef454ec03904bc19dc2a8ed27a28501
StefKoenig <stefanie.l.koenig@gmail.com>
2015-12-14 16:53:07 -0800

    Set new googlemaps in maps controller to an object pointer self.map. Delete unused libraries

--- public/js/mapController.js
+++ public/js/mapController.js
@@ -5,9 +5,10 @@
 	function mapController(){
 		var self = this
 		self.name = 'Map'
+		self.map = null
 
 		self.googleMap = function(){
-			  	new google.maps.Map(document.getElementById('map'), {
+			  self.map =	new google.maps.Map(document.getElementById('map'), {
 				    center: {lat: 34.0218579, lng: -118.5154401}, //34.0218579,-118.5154401
 				    zoom: 8
 		  })
