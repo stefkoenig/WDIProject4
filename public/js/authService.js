efa91e298a8a8e80d69ddc23cd1c08983c986f6f
StefKoenig <stefanie.l.koenig@gmail.com>
2015-12-16 11:44:08 -0800

    able to create a new user

--- public/js/authService.js
+++ public/js/authService.js
@@ -30,7 +30,7 @@
     }
     authFactory.getUser = function(){
         if ( AuthToken.getToken() )
-            return $http.get('/api/v1/me')
+            return $http.get('/api/v1/me', {cache: true})
         else
             return $q.reject({message: 'User has no token'})
 
@@ -51,7 +51,7 @@
 
         return config
     }
-    
+
     authIntercept.responseError = function(response){
         if ( response.status == 403 )
             $location.path('/login')
@@ -77,4 +77,37 @@
 
     return authTokenFactory
   }
+
+  function AuthInterceptor( $q, $location, AuthToken ){
+
+    var interceptorFactory = {};
+
+	// this will happen on all HTTP requests
+	interceptorFactory.request = function(config) {
+
+		// grab the token
+		var token = AuthToken.getToken();
+
+		// if the token exists, add it to the header as x-access-token
+		if (token)
+			config.headers['x-access-token'] = token;
+
+		return config;
+  }
+  // happens on response errors
+	interceptorFactory.responseError = function(response) {
+
+		// if our server returns a 403 forbidden response
+		if (response.status == 403) {
+			AuthToken.setToken();
+			$location.path('/login');
+		}
+
+		// return the errors from the server as a promise
+		return $q.reject(response);
+	}
+
+	return interceptorFactory;
+
+}
 }());
